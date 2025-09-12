8b28cc96a3b7b87d6fbe739956836c91e57f844e
MuskanMaddhesiya071 <muskanmadheshiya821@gmail.com>
2025-09-12 14:50:12 +0530

    Update mavenmovies assignment solution 5.sql

--- mavenmovies assignment solution 5.sql
+++ mavenmovies assignment solution 5.sql
@@ -27,3 +27,18 @@ on a.actor_id=fa.actor_id
 join film f
 on fa.film_id=f.film_id
 where concat(first_name, ' ', last_name) ='Will Wilson';
+
+-- Pull all the films which were rented and return in the month of May.
+select f.title as 'Rented and Returned in May'
+from mavenmovies.rental r 
+join mavenmovies.inventory i on r.inventory_id=i.inventory_id 
+join mavenmovies.film f on i.film_id=f.film_id 
+where r.rental_date like '__05_______________' and r.return_date like '__05_______________';
+
+
+-- Pull all the films with ‘Comedy’ category.
+select f.title as 'Comedy Category Movies'
+from mavenmovies.film f 
+join mavenmovies.film_category m on f.film_id=m.film_id
+join mavenmovies.category c on m.category_id=c.category_id
+where c.name = 'comedy';
