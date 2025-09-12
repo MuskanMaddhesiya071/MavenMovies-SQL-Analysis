cbee7df7a1b220681cf0ef05e043276a5c90431f
MuskanMaddhesiya071 <muskanmadheshiya821@gmail.com>
2025-09-12 10:26:36 +0530

    Add files via upload

---
+++ mavenmovies assignment solution 5.sql
@@ -0,0 +1,29 @@
+use mavenmovies; 
+-- Write a SQL query to count the number of characters except the spaces for each actor. Return first 10 actors name length along with their name.
+select concat(first_name, last_name) Fullname, length(concat(first_name, last_name)) name_length
+from actor
+order by name_length desc
+limit 10;
+
+-- List all Oscar awardees(Actors who received Oscar award) with their full names and length of their names.
+select concat(first_name, last_name) oscar_awardees, length(concat(first_name, last_name)) NameLength
+from actor_award
+where awards like '%Oscar%';
+ 
+-- Find the actors who have acted in the film ‘Frost Head’.
+select concat(first_name, last_name) 'Actor of film Frost Head'
+from actor a 
+join film_actor fa
+on a.actor_id=fa.actor_id
+join film f
+on f.film_id=fa.film_id
+where f.title like '%frost%head%';
+
+
+-- Pull all the films acted by the actor ‘Will Wilson’.
+select * from actor;
+
+
+
+
+
